66b4493630f71941c900ef2c468780dcc38633c3
filosuf <filosuf@mail.ru>
2022-05-31 11:33:23 +0300

    Initial Commit

---
+++ TimeToTravel/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TimeToTravel
+//
+//  Created by 1234 on 31.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
